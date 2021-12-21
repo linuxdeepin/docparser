908a80a24d19e9b5d702f0d4020a33def196e86a
Karo <liuzhangjian@uniontech.com>
2021-12-21 11:38:39 +0800

    fix: 全文搜索搜索失败
    txt类型文件解析时，换行内容未添加换行符
    
    Log: 修复全文搜索搜索失败问题
    Bug: https://pms.uniontech.com/zentao/bug-view-108896.html
    Change-Id: I02acd8374071d63ab21fc4a4b9237b070dcac8a3

--- src/fileext/txt/txt.cpp
+++ src/fileext/txt/txt.cpp
@@ -12,17 +12,18 @@
 #include "txt.hpp"
 
 namespace txt {
-Txt::Txt(const std::string& fileName)
-	: FileExtension(fileName) {}
+Txt::Txt(const std::string &fileName)
+    : FileExtension(fileName) {}
 
-int Txt::convert(bool addStyle, bool extractImages, char mergingMode) {
-	std::string line;
-	std::ifstream inputFile(m_fileName);
+int Txt::convert(bool addStyle, bool extractImages, char mergingMode)
+{
+    std::string line;
+    std::ifstream inputFile(m_fileName);
     while (getline(inputFile, line))
-        m_text += line;
+        m_text += line + '\n';
     inputFile.close();
 
     return 0;
 }
 
-}  // End namespace
+}   // End namespace
