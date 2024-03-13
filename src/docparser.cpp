1219fa3133a4067f4c613705c2185d921794ecac
nullptr <zhangsheng@uniontech.com>
2024-03-13 15:47:57 +0800

    feat: some formats supported (#24)
    Added support for some text formats
    
    Log: Added support for some text formats

--- src/docparser.cpp
+++ src/docparser.cpp
@@ -17,7 +17,26 @@
 
 #include <memory>
 #include <iostream>
-#include <string.h>
+#include <cstring>
+#include <unordered_set>
+
+static bool isValidSuffix(const std::unordered_set<std::string> &supportedSuffixs, const std::string &suffix)
+{
+    std::string lowercaseSuffix(suffix);
+    std::transform(lowercaseSuffix.begin(), lowercaseSuffix.end(), lowercaseSuffix.begin(), ::tolower);
+
+    return supportedSuffixs.count(lowercaseSuffix) > 0;
+}
+
+static bool isTextSuffix(const std::string &suffix)
+{
+    static const std::unordered_set<std::string> validSuffixes = {
+        "txt", "text", "md", "markdown", "sh", "html",
+        "xml", "xhtml", "json", "css", "yaml", "ini",
+        "bat", "js", "sql", "uof"
+    };
+    return isValidSuffix(validSuffixes, suffix);
+}
 
 std::string DocParser::convertFile(const std::string &filename)
 {
@@ -33,23 +52,19 @@ std::string DocParser::convertFile(const std::string &filename)
             document.reset(new docx::Docx(filename));
         } else if (!strcasecmp(suffix.c_str(), "pptx") || !strcasecmp(suffix.c_str(), "ppsx")) {
             document.reset(new pptx::Pptx(filename));
-        } else if (!strcasecmp(suffix.c_str(), "txt") || !strcasecmp(suffix.c_str(), "text") ||
-                   !strcasecmp(suffix.c_str(), "md") || !strcasecmp(suffix.c_str(), "markdown")) {
+        } else if (isTextSuffix(suffix)) {
             document.reset(new txt::Txt(filename));
-        } else if (!strcasecmp(suffix.c_str(), "doc") || !strcasecmp(suffix.c_str(), "dot") ||
-                   !strcasecmp(suffix.c_str(), "wps")) {
+        } else if (!strcasecmp(suffix.c_str(), "doc") || !strcasecmp(suffix.c_str(), "dot") || !strcasecmp(suffix.c_str(), "wps")) {
             document.reset(new doc::Doc(filename));
         } else if (!strcasecmp(suffix.c_str(), "rtf")) {
             document.reset(new rtf::Rtf(filename));
-        } else if (!strcasecmp(suffix.c_str(), "odg") || !strcasecmp(suffix.c_str(), "odt") ||
-                   !strcasecmp(suffix.c_str(), "ods") || !strcasecmp(suffix.c_str(), "odp")) {
+        } else if (!strcasecmp(suffix.c_str(), "odg") || !strcasecmp(suffix.c_str(), "odt") || !strcasecmp(suffix.c_str(), "ods") || !strcasecmp(suffix.c_str(), "odp")) {
             document.reset(new odf::Odf(filename));
         } else if (!strcasecmp(suffix.c_str(), "xls") || !strcasecmp(suffix.c_str(), "xlsx")) {
             document.reset(new excel::Excel(filename, suffix));
         } else if (!strcasecmp(suffix.c_str(), "xlsb")) {
             document.reset(new xlsb::Xlsb(filename));
-        } else if (!strcasecmp(suffix.c_str(), "ppt") || !strcasecmp(suffix.c_str(), "pps") ||
-                   !strcasecmp(suffix.c_str(), "dps")) {
+        } else if (!strcasecmp(suffix.c_str(), "ppt") || !strcasecmp(suffix.c_str(), "pps") || !strcasecmp(suffix.c_str(), "dps") || !strcasecmp(suffix.c_str(), "pot")) {
             document.reset(new ppt::Ppt(filename));
         } else if (!strcasecmp(suffix.c_str(), "pdf")) {
             document.reset(new pdf::Pdf(filename));
@@ -59,7 +74,7 @@ std::string DocParser::convertFile(const std::string &filename)
 
         document->convert();
         content = document->m_text;
-    } catch(const std::logic_error &error) {
+    } catch (const std::logic_error &error) {
         std::cout << error.what() << std::endl;
     } catch (...) {
         std::cerr << "Parse failed: " << filename << std::endl;
